1bd775ca6dff96832c3b37096d7675fcc9a5f8d2
Gyultaengshin <aajkrvv@gmail.com>
2026-05-24 19:05:21 +0900

    update web-digital-wellbeing-script.user.js

--- web-digital-wellbeing-script.user.js
+++ web-digital-wellbeing-script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Web Digital Wellbeing Script
 // @namespace    aajkrvv
-// @version      1.1.0
+// @version      1.1.1
 // @description  This is a script that is currently not working.
 // @author       aajkrvv
 // @match        *://*/*
