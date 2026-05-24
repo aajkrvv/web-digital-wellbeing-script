64f6360f20a962ab509bfee8577296f77da01971
Gyultaengshin <aajkrvv@gmail.com>
2026-05-24 17:08:41 +0900

    update web-digital-wellbeing-script.user.js

--- web-digital-wellbeing-script.user.js
+++ web-digital-wellbeing-script.user.js
@@ -1,1 +1,18 @@
-1
+// ==UserScript==
+// @name         Digital Wellbeing
+// @namespace    aajkrvv
+// @version      1.0.0
+// @description  도
+// @author       aajkrvv
+// @match        *://*/*
+// @exclude      https://example.com/*
+// @run-at       document-start
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_deleteValue
+// @connect      *
+// @homepageURL  https://aajkrvv.netlify.app/digital-wellbeing
+// @supportURL   https://aajkrvv.netlify.app/
+// @updateURL    https://example.com/script.meta.js
+// @downloadURL  https://example.com/script.user.js
+// ==/UserScript==
